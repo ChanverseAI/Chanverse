4e3af0a31c14b3ae34b9b8af52b83e7f369a0078
Mejd B <chanverse.ai@gmail.com>
2025-05-25 21:10:02 +0200

    Ready for Vercel deployment

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,12 @@
-export default function Home() { 
-  return <h1 style={{ padding: "2rem" }}>Chanverse is Back!</h1> 
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Chanverse</h1>
+      <Link href="/generate">
+        <button>Start Generating</button>
+      </Link>
+    </div>
+  )
 }
